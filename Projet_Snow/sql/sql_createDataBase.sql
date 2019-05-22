c01e9d0ba6618609bff19f6f4efbc52df31d3126
ThomasCPNV <Thomas.Huguet@cpnv.ch>
2019-05-22 20:44:34 +0200

    Correction MCD + MLD, script createDatabase fonctionnel

--- Projet_Snow/sql/sql_createDataBase.sql
+++ Projet_Snow/sql/sql_createDataBase.sql
@@ -5,20 +5,14 @@
 -- HeidiSQL Version:             9.5.0.5196
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
-
 -- Export de la structure de la base pour snows
-CREATE DATABASE IF NOT EXISTS `snows` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+DROP DATABASE IF EXISTS `snows`;
+CREATE DATABASE `snows`;
 USE `snows`;
 
 -- Export de la structure de la table snows. snows
 CREATE TABLE IF NOT EXISTS `snows` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `code` varchar(4) NOT NULL,
   `brand` varchar(20) NOT NULL,
   `model` varchar(30) NOT NULL,
@@ -30,11 +24,10 @@ CREATE TABLE IF NOT EXISTS `snows` (
   `active` tinyint(4) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `snow_code` (`code`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=UTF8 COLLATE=utf8_unicode_ci;
+);
 
 -- Export de données de la table snows.snows : ~9 rows (environ)
-DELETE FROM `snows`;
-/*!40000 ALTER TABLE `snows` DISABLE KEYS */;
+
 INSERT INTO `snows` (`id`, `code`, `brand`, `model`, `snowLength`, `qtyAvailable`, `description`, `dailyPrice`, `photo`, `active`) VALUES
 	(1, 'B101', 'Burton', 'Custom', 160, 22, 'La board la plus fiable de tous les temps, la solution snowboard pour tous les terrains. (Homme)', 29, 'view/content/images/B101_small.jpg', 1),
 	(2, 'B126', 'Burton', 'Free Thinker', 165, 2, 'Élargissez votre vision grâce son interprétation du ride tout terrain dynamique sur la poudreuse. (Homme)', 45, 'view/content/images/B126_small.jpg', 1),
@@ -45,7 +38,6 @@ INSERT INTO `snows` (`id`, `code`, `brand`, `model`, `snowLength`, `qtyAvailable
 	(7, 'P067', 'Prior', 'Brandwine 153', 154, 9, 'High performance, directional Freeride board, draws a smooth, stable and fast line through all snow conditions. (Femme)', 49, 'view/content/images/P067_small.jpg', 1),
 	(8, 'P165', 'Prior', 'BC Split 161', 169, 1, 'Sa forme directionnelle Freeride offre une ride plutôt douce et stable dans une variété de conditions', 35, 'view/content/images/P165_small.jpg', 1),
 	(9, 'K409', 'K2', 'Lime Lite', 149, 15, 'Best For Freestyle Evolution with a Focus on Fun (Femme)', 55, 'view/content/images/K409_small.jpg', 1);
-/*!40000 ALTER TABLE `snows` ENABLE KEYS */;
 
 -- Export de la structure de la table snows. users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -55,25 +47,21 @@ CREATE TABLE IF NOT EXISTS `users` (
   `pseudo` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `userEmailAddress` (`userEmailAddress`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
--- Export de la structure de la table snows. leazings
-CREATE TABLE IF NOT EXISTS `leazings` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `idUsers` int(10) unsigned NOT NULL,
-  `idSnows` int(10) unsigned NOT NULL,
+-- Export de la structure de la table snows. leasings
+CREATE TABLE IF NOT EXISTS `leasings` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `idUsers` int(10) UNSIGNED NOT NULL,
+  `idSnows` int(10) UNSIGNED NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 -- Add foreign keys in table
-Alter TABLE `leazings`
+Alter TABLE `leasings`
 Add foreign key (`idUsers`) REFERENCES `users`(`id`);
 
-Alter TABLE `leazings`
+Alter TABLE `leasings`
 Add foreign key (`idSnows`) REFERENCES `snows`(`id`);
