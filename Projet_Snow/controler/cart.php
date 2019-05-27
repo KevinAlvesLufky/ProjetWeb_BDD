dd9c2a1c9b824b5b0f1790a6c3213ba71794bf28
1888DCT <47389547+1888DCT@users.noreply.github.com>
2019-05-27 11:32:29 +0200

    debug

--- Projet_Snow/controler/cart.php
+++ Projet_Snow/controler/cart.php
@@ -91,12 +91,8 @@ function deleteCartRequest($snowLine)
 {
         if (isset($snowLine))
         {
-          /**
-          *TODO debug du deletcart, commande de base :
-          * array_splice($_SESSION['cart'],$snowLine,1);
-          */
             //Reorders the data of selected snow
-            array_splice($_SESSION['cart'],$snowLine,0);
+            array_splice($_SESSION['cart'],$snowLine,1);
 
             // Test if the cart is empty
             if (count($_SESSION['cart'])<1)
