31015bd1d70564a3f4d9d9cb194fcda79ce6ac84
Thomas <Thomas.Huguet@cpnv.ch>
2019-06-07 09:31:02 +0200

    correction page admin Snows

--- Projet_Snow/view/snowsSeller.php
+++ Projet_Snow/view/snowsSeller.php
@@ -43,32 +43,3 @@ ob_start();
 $content = ob_get_clean();
 require 'gabarit.php';
 ?>
-
-
-
-    <div  class="table-responsive">
-        <table class="table table-dark">
-            <thead class="thead-dark">
-            <tr>
-                <th>Code</th>
-                <th>Marque</th>
-                <th>Modèle</th>
-                <th>Longueur</th>
-                <th>Prix</th>
-                <th>Disponibilité</th>
-                <th>Photo</th>
-            </tr>
-            </thead>
-            <?php foreach ($snowsResults as $snow):?>
-                <tr>
-                    <td><a href="index.php?action=displayASnow&code=<?= $snow['code']; ?>"><?php echo $snow['code'] ?></a></td>
-                    <td><?php echo $snow['brand'] ?></td>
-                    <td><?php echo $snow['model'] ?></td>
-                    <td><?php echo $snow['snowLength'] ?> cm</td>
-                    <td>CHF <?php echo $snow['dailyPrice'] ?>.- par jour</td>
-                    <td><?php echo $snow['qtyAvailable'] ?></td>
-                    <td><img src="<?php echo $snow['photo']; ?>" alt="<?= $snow['code']; ?>" style="width: 10%;"></td>
-                </tr>
-            <?php endforeach ?>
-        </table>
-    </div>
